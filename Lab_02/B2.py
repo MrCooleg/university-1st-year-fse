f1ee875fae353e8c81f51b13da9bb723e69de8bb
Дикевич Егор <egordikevich2008@gmail.com>
2026-09-20 21:18:19 +0300

    Lab_02 коммит B2.2
    Рассчитываем значения

--- Lab_02/B2.py
+++ Lab_02/B2.py
@@ -1,3 +1,9 @@
+#возьмем функцию округления из решения прошлой задачи и доработаем:
+#теперь корректно работает для отрицательных чисел
+def school_round(num: float, digits: int):
+    factor = 10 ** digits
+    return round(int(abs(num) * factor + 0.5) * ((num > 0) - (num < 0)) / factor, digits)
+
 #открываем файл и записываем его строки в lines
 with open('1.WCData.txt', 'r') as file1:
     lines = file1.read().splitlines()
@@ -12,3 +18,11 @@ for i in range(2, len(lines)):
     time.append(lines[i].split()[0])
     air_temp.append(int(lines[i].split()[1]))
     wind_speed.append(int(lines[i].split()[2]))
+
+wc_temp = []
+wc_effect = []
+
+#рассчитываем температуру WC и WC Effect и записываем в соответствующие значения
+for i in range(len(time)):
+    wc_temp.append(school_round(35.74 + 0.6125 * air_temp[i] + (0.4275 * air_temp[i] - 35.75) * wind_speed[i] ** 0.16, 1))
+    wc_effect.append(school_round(wc_temp[i] - air_temp[i], 1))
